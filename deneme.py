9b870a55d70157374c04255dd0815f00e7ec1005
Ercan Polat <plt.ercan.1661@gmail.com>
2026-02-16 12:22:34 +0300

    python donguler

---
+++ deneme.py
@@ -0,0 +1,47 @@
+# my_list = [1,3,"a"]
+# print(type(my_list))
+# my_tuple = (1,3,"a")
+# print(type(my_tuple))
+# my_set = {1,3,"a"}
+# print(type(my_set))
+# my_dict = {"a":1,"b":3}
+# print(type(my_dict))
+
+# my_superhero = input("enter superhero: ")
+# my_superhero_list = ["Spider Man","Iron Man","Thor","Black Widow"]
+# if my_superhero in my_superhero_list:
+#     print(":)")
+# else:
+#     print(":(")
+
+liste = [10,20,30,40,50,60,70,]
+
+for l in liste:
+    if l%6==0:
+        print(l)
+
+my_tuple_list = [(0,1,2),(3,4,5),(6,7,8),(9,10,11)]
+len(my_tuple_list)
+#4
+for (x,y,z) in my_tuple_list:
+    print(x)
+# 0
+# 3
+# 6
+# 9
+
+
+my_dictionary = {"k1":100,"k2":200,"k3":300}
+
+my_dictionary.items()
+dict_items([('k1', 100), ('k2', 200), ('k3', 300)])
+for (key,value) in my_dictionary.items():
+    print(value)
+# 100
+# 200
+# 300
+for num in my_dictionary.values():
+    print(num)
+# 100
+# 200
+# 300
